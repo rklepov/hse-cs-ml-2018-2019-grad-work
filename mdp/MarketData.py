fe021d5f246f40b0807249586f0c8490c643ecef
Roman Klepov <rklepov@gmail.com>
2019-12-12 13:28:44 +0300

    Amend market data processing library
    -  attributes are now accessed via properties
    
    -  implemented generators for regression, classification and multitask
    modes (they share the common logic in the base class)
    
    -  fixed the calculation of some indicators
    
    -  added the function to recover price value from the logarithmic
    returns

--- mdp/MarketData.py
+++ mdp/MarketData.py
@@ -1,10 +1,10 @@
 # mdp/MarketData.py
 
 import os
-import numpy as np
 
-import talib
+import numpy as np
 import pandas_datareader as pdr
+import talib
 
 from . import Utils
 
@@ -14,11 +14,104 @@ class MarketData(object):
     """
 
     def __init__(self, **kwargs):
-        Utils.set_self_attr(self, **kwargs)
-        self.src_feature_accessors = [f'get_{k}' for k in kwargs.keys() if k not in ('instrument', 'timestamps', 'c')]
+        Utils.set_self_attr(self, __class__, **kwargs)
+        # в качсестве признаков используем логарифмические доходности, а не сами цены
+        self.__feature_names = [k for k in kwargs.keys() if k not in ['instrument', 'timestamps'] + list('ohlc')]
 
     def __len__(self):
-        return len(self.c_)
+        return len(self.c)
+
+    def get_close_price_from_log_ret(self, c_slice, log_ret):
+        """ Переход от предсказанных логарифмических доходностей к ценам.
+
+            Предсказанная доходность следующего периода умножается на реальную цену предыдущего периода, тем самым
+            получаем предсказанную цену следующего периода.
+        """
+        return self.c[c_slice] * np.exp(log_ret).squeeze()
+
+    @property
+    def feature_names(self):
+        return self.__feature_names
+
+    @property
+    def instrument(self):
+        return self.__instrument
+
+    @property
+    def timestamps(self):
+        return self.__timestamps
+
+    @property
+    def o(self):
+        return self.__o
+
+    @property
+    def o_log_ret(self):
+        return self.__o_log_ret
+
+    @property
+    def h(self):
+        return self.__h
+
+    @property
+    def h_log_ret(self):
+        return self.__h_log_ret
+
+    @property
+    def l(self):
+        return self.__l
+
+    @property
+    def l_log_ret(self):
+        return self.__l_log_ret
+
+    @property
+    def c_log_ret(self):
+        return self.__c_log_ret
+
+    @property
+    def c(self):
+        return self.__c
+
+    @property
+    def v(self):
+        return self.__v
+
+    @property
+    def bband20_lower(self):
+        return self.__bband20_lower
+
+    @property
+    def bband20_middle(self):
+        return self.__bband20_middle
+
+    @property
+    def bband20_upper(self):
+        return self.__bband20_upper
+
+    @property
+    def ema14(self):
+        return self.__ema14
+
+    @property
+    def ema30(self):
+        return self.__ema30
+
+    @property
+    def macd(self):
+        return self.__macd
+
+    @property
+    def macd_signal(self):
+        return self.__macd_signal
+
+    @property
+    def rsi14(self):
+        return self.__rsi14
+
+    @property
+    def willr14(self):
+        return self.__willr14
 
     @classmethod
     def create_(cls, instrument, df, timestamps, **kwargs):
@@ -28,16 +121,13 @@ class MarketData(object):
             на соответствующие имена колонок датафрейма df.
         """
         # [скорректированная] цена закрытия - наш главный признак для вычисления индикаторов
-        open_price, high_price, low_price, close_price = [df[kwargs[k]].values for k in 'ohlc']
+        init_kwargs = {'instrument': instrument, 'timestamps': timestamps}
+        init_kwargs.update({k: df[kwargs[k]].values for k in 'ohlcv'})
+        open_price, high_price, low_price, close_price = [init_kwargs[k] for k in 'ohlc']
 
-        init_kwargs = {'instrument': instrument, 'timestamps': timestamps, 'c': close_price}
-
-        for k, v in kwargs.items():
-            if k in ['o', 'h', 'l', 'c']:
-                # будем работать с логарифмической доходностью
-                init_kwargs[f'{k}_logret'] = cls.log_returns(df[v]).values
-            else:
-                init_kwargs[k] = df[v].values
+        for k in 'ohlc':
+            # будем работать с логарифмической доходностью
+            init_kwargs[f'{k}_log_ret'] = cls.log_returns(df[kwargs[k]]).values
 
         # добавим индикаторы
         # TODO: динамическая настройка списка индикаторов?
@@ -68,6 +158,24 @@ class MarketData(object):
         return cls.create_(instrument, df, df.index.get_level_values('date').values,
                            **{'o': 'adjOpen', 'h': 'adjHigh', 'l': 'adjLow', 'c': 'adjClose', 'v': 'adjVolume'})
 
+    @classmethod
+    def create_from_quandl(cls, instrument, *args, **kwargs):
+        """ Загрузка данных через pandas_datareader is https://www.quandl.com
+        """
+        df = pdr.get_data_quandl(instrument, api_key=os.environ.get('QUANDL_API_KEY'), *args, **kwargs)
+        return cls.create_(instrument, df, df.index.values,
+                           **{'o': 'AdjOpen', 'h': 'AdjHigh', 'l': 'AdjLow', 'c': 'AdjClose', 'v': 'AdjVolume'})
+
+    @classmethod
+    def create_from_alphavantage_intraday(cls, instrument, *args, **kwargs):
+        """ Загрузка данных через pandas_datareader is https://www.alphavantage.co
+            У них есть внутридневные (минутные) данные, но только за неделю назад
+        """
+        df = pdr.get_data_alphavantage(instrument, api_key=os.environ.get('ALPHAVANTAGE_API_KEY'),
+                                       function='TIME_SERIES_INTRADAY')
+        return cls.create_(instrument, df, df.index.values,
+                           **{'o': 'open', 'h': 'high', 'l': 'low', 'c': 'close', 'v': 'volume'})
+
     @staticmethod
     def log_returns(series):
         """ Вычисление логарифмических доходностей.
@@ -84,8 +192,8 @@ class MarketData(object):
     def indi_macd(p, fast=12, slow=26, signal=9):
         """ Moving Average Convergence Divergence
         """
-        return zip([f'macd_{s}' for s in [f'fast{fast}', f'slow{slow}', f'signal{signal}']],
-                   talib.MACD(p, fast, slow, signal))
+        return zip(['macd', 'macd_signal'],
+                   talib.MACD(p, fast, slow, signal)[:2])
 
     @staticmethod
     def indi_rsi(p, period=14):
@@ -97,7 +205,7 @@ class MarketData(object):
     def indi_bband(p, timeperiod=20, nbdevup=2, nbdevdn=2, matype=0):
         """ Bollinger Bands
         """
-        return zip([f'bband{timeperiod}_{s}' for s in ['upper', 'middle', 'lowerband']],
+        return zip([f'bband{timeperiod}_{s}' for s in ['upper', 'middle', 'lower']],
                    talib.BBANDS(p, timeperiod, nbdevup, nbdevdn, matype))
 
     @staticmethod
--- mdp/MovingWindowFeatures.py
+++ mdp/MovingWindowFeatures.py
@@ -10,10 +10,38 @@ class MovingWindowFeatures(object):
     """
 
     def __init__(self, **kwargs):
-        Utils.set_self_attr(self, **kwargs)
+        Utils.set_self_attr(self, __class__, **kwargs)
 
     def __len__(self):
-        return len(self.features_)
+        return len(self.features)
+
+    @property
+    def market_data(self):
+        return self.__market_data
+
+    @property
+    def feature_names(self):
+        return self.__feature_names
+
+    @property
+    def features(self):
+        return self.__features
+
+    @property
+    def target(self):
+        return self.__target
+
+    @property
+    def window_size(self):
+        return self.__window_size
+
+    @property
+    def forecast_offset(self):
+        return self.__forecast_offset
+
+    @property
+    def start_ix(self):
+        return self.__start_ix
 
     @classmethod
     def get_target_direction(cls, target):
@@ -26,17 +54,22 @@ class MovingWindowFeatures(object):
         return (features - mean[:, np.newaxis, :]) / std[:, np.newaxis, :]
 
     @classmethod
-    def create(cls, marketData, window=30, forecast=1):
+    def create(cls, market_data, selected_features=None, window_size=30, forecast_offset=1):
         """
             TODO: Исходим из предположения, что исходные данные адекватного размера,
-            поэтому крайние случаи (условия вроде window > len(marketData), batch_size > 0 и т.п.) не проверяются.
+            поэтому крайние случаи (условия вроде window > len(market_data), batch_size > 0 и т.п.) не проверяются.
         """
+        if selected_features is None:
+            selected_features = market_data.feature_names
+        else:
+            selected_features = list({f for f in market_data.feature_names if f in selected_features})
+
         # признаки: движущиеся окна временных рядов цен и индикаторов
-        features = np.column_stack([getattr(marketData, f)() for f in marketData.src_feature_accessors])
+        features = np.column_stack([getattr(market_data, f) for f in selected_features])
 
         # целевая переменная регрессии: логарифмическая доходность
         # целевая переменная классификации: (1) - доходность >0, (0) доходность <= 0
-        target = marketData.get_c_logret()
+        target = market_data.c_log_ret
 
         # индикаторы могут содержать некоторое количество NaN в начале рядов
         # для простоты найдём максимальный индекс позиции NaN среди всех рядов
@@ -49,10 +82,12 @@ class MovingWindowFeatures(object):
         # признаками будут последовательности "окон" для каждого временного ряда
         time_series_features = np.lib.stride_tricks.as_strided(
             features,
-            shape=(features.shape[0] + 1 - window - forecast, window, features.shape[1]),
+            shape=(features.shape[0] + 1 - window_size - forecast_offset, window_size, features.shape[1]),
             strides=(features.strides[0], features.strides[0], features.strides[1]))
 
-        return cls(features=time_series_features, target=target, window=window,
-                   forecast_offset=forecast, start_ix=start_ix)
+        return cls(market_data=market_data,
+                   feature_names=selected_features, features=time_series_features,
+                   target=target, window_size=window_size,
+                   forecast_offset=forecast_offset, start_ix=start_ix)
 
 # __EOF__
